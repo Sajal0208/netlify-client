500a90dd4b87c833d6ac51ca9af875fdaf6813c2
Sajal0208 <sajaldewangan@gmail.com>
2024-03-07 06:13:46 +0530

    Added scroll, tabs and multi-step-loader functionality

--- tailwind.config.js
+++ tailwind.config.js
@@ -72,5 +72,19 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    addVariablesForColors,
+  ],
+}
+
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
 }
