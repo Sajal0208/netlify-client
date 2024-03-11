fffb3efc84b67824f8ca3a3b55f5449bff0968c9
Sajal0208 <sajaldewangan@gmail.com>
2024-03-11 18:03:59 +0530

    Added toast (sonner)

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import App from './App';
+import { Toaster } from 'sonner';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -17,6 +18,7 @@ root.render(
         <Routes>
           <Route path="/*" element={<App />} />
         </Routes>
+        <Toaster />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
